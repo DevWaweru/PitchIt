8e41f65c2f1a01521f6ab30199ae12cefb40be52
Richard <richi@localhost.localdomain>
2018-05-18 16:13:22 +0300

    Initializing the app blueprints and modules

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
+simple = SimpleMDE
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
