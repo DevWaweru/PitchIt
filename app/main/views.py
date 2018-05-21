c7fbadbfa4311addd948dbc7a720e7286094c581
Richard <richi@localhost.localdomain>
2018-05-21 10:23:37 +0300

    Adding stats to the user profiles

--- app/main/views.py
+++ app/main/views.py
@@ -14,16 +14,22 @@ def index():
 
     return render_template("index.html", title=title)
 
-@main.route('/user/<uname>')
+@main.route('/user/<uname>&<id_user>')
 @login_required
-def profile(uname):
+def profile(uname, id_user):
     user = User.query.filter_by(username = uname).first()
 
     title = f"{uname.capitalize()}'s Profile"
+
+    get_pitches = Pitch.query.filter_by(user_id = id_user).all()
+    get_comments = Comment.query.filter_by(user_id = id_user).all()
+    get_upvotes = UpVote.query.filter_by(id_user = id_user).all()
+    get_downvotes = DownVote.query.filter_by(id_user = id_user).all()
+
     if user is None:
         abort(404)
     
-    return render_template('profile/profile.html', user = user, title=title)
+    return render_template('profile/profile.html', user = user, title=title, pitches_no = get_pitches, comments_no = get_comments, likes_no = get_upvotes, dislikes_no = get_downvotes)
 
 @main.route('/home/like/<int:id>', methods = ['GET','POST'])
 @login_required
